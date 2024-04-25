dcc7279891583093ea9c469be18b8917e183269d
kirkcabbage <kirkcabbage@proton.me>
2024-04-25 16:55:10 +0200

    create html

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+Savoir quand l'user clique sur jouer
+Afficher 1re question
+Connaître réponse
+Afficher un message ou l'autre selon réponse
+Savoir quand user clique sur question suvante
+Afficher question suivante
+Savoir quand toutes les questions ont été faites
+Afficher dernière page
+
+récup réponse formulaire (front)
+event listener
+afficher/masquer element html
+window.location
+*/
